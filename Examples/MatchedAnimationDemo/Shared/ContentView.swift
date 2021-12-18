75b431fb34376098224f734d37c551a7c62c4476
Ryan Carver <ryan@ryancarver.com>
2021-12-17 21:35:55 -0800

    working example

---
+++ Examples/MatchedAnimationDemo/Shared/ContentView.swift
@@ -0,0 +1,105 @@
+import SwiftUI
+import MatchedAnimation
+
+struct ContentView: View {
+
+    @State var flag: Bool = false
+    @State var animation: Animation? = .easeInOut(duration: 1)
+
+    var size: CGSize {
+        CGSize(width: 40, height: 50)
+    }
+    func position(in container: CGSize) -> CGPoint {
+        CGPoint(
+            x: flag ? container.width - self.size.width : 0,
+            y: 0
+        )
+    }
+
+    var body: some View {
+        ZStack {
+            GeometryReader { g in
+                VStack {
+                    Group {
+                        SwiftUIBox(
+                            size: self.size,
+                            position: self.position(in: g.size)
+                        )
+                        UIKitBox(
+                            size: self.size,
+                            position: self.position(in: g.size)
+                        )
+                    }
+                    .frame(height: self.size.height)
+                    .background(Color.primary.opacity(0.1))
+                }
+            }
+            .padding(.vertical, 100)
+            HStack {
+                Button {
+                    withAnimation(animation) {
+                        flag.toggle()
+                    }
+                } label: { Text("Flip") }
+            }
+            .frame(maxHeight: .infinity, alignment: .bottom)
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct SwiftUIBox: View {
+    let size: CGSize
+    let position: CGPoint
+    var body: some View {
+        Rectangle()
+            .foregroundColor(.blue)
+            .frame(width: size.width, height: size.height)
+            .position(x: position.x + (size.width/2), y: position.y + (size.height/2))
+    }
+}
+
+#if canImport(UIKit)
+struct UIKitBox: UIViewRepresentable {
+    let size: CGSize
+    let position: CGPoint
+    func makeUIView(context: Context) -> UIView {
+        let view = UIView(frame: .zero)
+        let box = UIView()
+        box.backgroundColor = .red
+        view.addSubview(box)
+        return view
+    }
+    func updateUIView(_ view: UIView, context: Context) {
+        withMatchedAnimation(context.transaction.animation) {
+            view.subviews.first?.frame = CGRect(origin: position, size: size)
+        }
+    }
+}
+#endif
+
+#if canImport(AppKit)
+struct UIKitBox: NSViewRepresentable {
+    let size: CGSize
+    let position: CGPoint
+    func makeNSView(context: Context) -> NSView {
+        let view = NSView(frame: .zero)
+        let box = NSView()
+        box.wantsLayer = true
+        box.layer?.backgroundColor = NSColor.red.cgColor
+        view.addSubview(box)
+        return view
+    }
+    func updateNSView(_ view: NSView, context: Context) {
+        withMatchedAnimation(context.transaction.animation) {
+            view.subviews.first?.frame = CGRect(origin: position, size: size)
+        }
+    }
+}
+#endif
---
+++ Examples/MatchedAnimationDemo/Shared/MatchedAnimationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchedAnimationDemoApp.swift
+//  Shared
+//
+//  Created by Ryan Carver on 12/17/21.
+//
+
+import SwiftUI
+
+@main
+struct MatchedAnimationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
